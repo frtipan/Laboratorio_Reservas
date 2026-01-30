5e1fd4deba2313c446ce663b98c0cd835de96b88
Fernando Tipan <93298810+frtipan@users.noreply.github.com>
2026-01-29 23:28:31 -0500

    Pruebas

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Definición del esquema del usuario
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true }, // Correo único
-  password: { type: String, required: true } // Contraseña hasheada
+  password: { type: String, required: true } 
 });
 
 // Exportación del modelo User
